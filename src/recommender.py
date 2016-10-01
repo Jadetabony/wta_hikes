4677f13f692f893864c8b6cea318d3c791c4effc
Jade Tabony <jat257@cornell.edu>
2016-10-01 16:31:15 -0700

    finalized itemcontent recommender code

--- src/recommender.py
+++ src/recommender.py
@@ -1,4 +1,4 @@
-""""DOC strings"""
+"""DOC strings"""
 
 # import packages
 import pandas as pd
@@ -12,25 +12,26 @@ class hikeRecommender(object):
     def __init__(self, hikes, weights=None):
         self.hike_matrix = hikes
         self.feature_weights = weights
-        self.hikes_liked = None
+        self.hikes_liked = []
         self.hikes_disliked = []
 
-    def likeHike(hike_id):
-        if not self.hikes_liked:
-            self.hikes_liked = self.hike_matrix.ix[hike_id]
-            self.hike_matrix = self.hike_matrix.drop(hike_id, axis=0).reset_index(drop=True)
+    def likeHike(self, hike_id):
+        indx = self.hike_matrix[self.hike_matrix['hike_id']==hike_id].index[0]
+        if len(self.hikes_liked)==0:
+            self.hikes_liked = pd.DataFrame(self.hike_matrix.ix[indx]).transpose()
+            self.hike_matrix = self.hike_matrix.drop(indx, axis=0).reset_index(drop=True)
         else:
-            self.hikes_liked = self.hike_matrix.ix[hike_id]
-            self.hike_matrix = self.hike_matrix.drop(hike_id, axis=0).reset_index(drop=True)
+            self.hikes_liked = self.hikes_liked.append(self.hike_matrix.ix[indx])
+            self.hike_matrix = self.hike_matrix.drop(indx, axis=0).reset_index(drop=True)
 
-    def recommend(n=5):
+    def recommend(self, n=5):
         # Calculate similarity to all of the hikes
         # average similarities
         # return top 5
-        indx_id = self.hike_matrix('hike_id')
+        indx_id = self.hike_matrix['hike_id']
         X = self.hike_matrix.drop('hike_id', axis=1)
         y = self.hikes_liked.drop('hike_id', axis=1)
         cs = cosine_similarity(X, y).mean(axis=1)
-        rec_index= np.argsort(cs[-n:][::-1])
+        rec_index= np.argsort(cs)[-n:][::-1]
         recommendations = indx_id.ix[rec_index]
         return recommendations
