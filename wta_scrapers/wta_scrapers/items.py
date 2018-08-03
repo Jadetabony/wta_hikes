f8cb10b854392d40118db783bd8304056982b42e
Jade Tabony <jat257@cornell.edu>
2018-08-03 11:51:15 -0700

    Update item names. Add trip report items

--- wta_scrapers/wta_scrapers/items.py
+++ wta_scrapers/wta_scrapers/items.py
@@ -24,20 +24,20 @@ class WtaScrapersItem(scrapy.Item):
                                output_processor=Join())
     TimeFromSeattle = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
                           output_processor=Join())
-    Coast = scrapy.Field()
-    DogsAllowed = scrapy.Field()
-    EstablishedCampsites = scrapy.Field()
-    FallFoliage = scrapy.Field()
-    GoodForKids = scrapy.Field()
-    Lakes = scrapy.Field()
-    MountainViews = scrapy.Field()
-    OldGrowth = scrapy.Field()
-    RidgesPasses = scrapy.Field()
-    Rivers = scrapy.Field()
-    Summits = scrapy.Field()
-    Waterfalls = scrapy.Field()
-    WildflowersMeadows = scrapy.Field()
-    Wildlife = scrapy.Field()
+    Coast = scrapy.Field(output_processor=Join())
+    DogsAllowed = scrapy.Field(output_processor=Join())
+    EstablishedCampsites = scrapy.Field(output_processor=Join())
+    FallFoliage = scrapy.Field(output_processor=Join())
+    GoodForKids = scrapy.Field(output_processor=Join())
+    Lakes = scrapy.Field(output_processor=Join())
+    MountainViews = scrapy.Field(output_processor=Join())
+    OldGrowth = scrapy.Field(output_processor=Join())
+    RidgesPasses = scrapy.Field(output_processor=Join())
+    Rivers = scrapy.Field(output_processor=Join())
+    Summits = scrapy.Field(output_processor=Join())
+    Waterfalls = scrapy.Field(output_processor=Join())
+    WildflowersMeadows = scrapy.Field(output_processor=Join())
+    Wildlife = scrapy.Field(output_processor=Join())
     Url = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
                           output_processor=Join())
     Rating = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
@@ -50,13 +50,21 @@ class WtaScrapersItem(scrapy.Item):
                           output_processor=Join())
 
     # trip reports
-    CreateDate = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+    DateHiked = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
                              output_processor=Join())
-    ReviewText = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+    ReportText = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
                                 output_processor=Join())
+    TrailConditions = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+                                output_processor=Join())
+    Road = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+                              output_processor=Join())
+    Bugs = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+                              output_processor=Join())
+    Snow = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+                                   output_processor=Join())
 
     # Authors
     MemberSince = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
                           output_processor=Join())
-    Name = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
+    AuthorName = scrapy.Field(input_processor=MapCompose(lambda x: x.lstrip().rstrip()),
                            output_processor=Join())
